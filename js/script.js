ef0cc349833ba0aab90436d15dd7bb7e93ba9def
Yésica González <yesigonzalez107@gmail.com>
2025-08-19 16:23:44 -0300

    Ahora se muestran las personas cargadas en el json/data.json

--- js/script.js
+++ js/script.js
@@ -19,3 +19,15 @@ function showData(dataArray) {
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+fetch(DATA_URL)
+  .then( response => response.json() )
+  .then( data => {
+    if(data){
+      // console.log(data);
+      const estudiantes = data.students;
+      showData(estudiantes);
+    }
+  })
+  .catch( error => {
+    console.log("Error", error.message);
+  });
